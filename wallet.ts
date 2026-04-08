9ae8ae255367c65bdcb57f730df8ba91624d74c9
alameri-ui <ye734999901@gmail.com>
2026-04-08 04:20:30 +0300

    Delete wallet.ts

--- wallet.ts
+++
@@ -1,7 +0,0 @@
-import { api } from "../lib/api";
-
-export const getOrders = (page = 1) =>
-  api.get(`/api/orders?page=${page}`).then(r => r.data);
-
-export const updateOrderStatus = (id: number, status: string) =>
-  api.patch(`/api/orders/${id}/status`, { status }).then(r => r.data);
